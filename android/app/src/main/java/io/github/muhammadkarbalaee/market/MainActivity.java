a7b733f94c5ea6898afa0758463fcd4f2248b6e7
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2022-05-09 21:55:11 +0430

    initial commit

---
+++ android/app/src/main/java/io/github/muhammadkarbalaee/market/MainActivity.java
@@ -0,0 +1,6 @@
+package io.github.muhammadkarbalaee.market;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text('Market'),
+        ),
+      ),
+    )
+  );
+}
