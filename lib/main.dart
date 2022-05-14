0e592fc3de22123b5112e817ffa3d5c0e3d2480e
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2022-05-14 23:35:47 +0430

    learned localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(
     MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('fa', ''),
+      ],
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
